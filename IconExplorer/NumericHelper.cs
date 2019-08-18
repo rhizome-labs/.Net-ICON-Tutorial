2f515ef992f0102fcf5fee00566c15e460bfa3fa
Andrew Burns <anditburns@gmail.com>
2019-08-18 15:14:15 +0800

    basic console application menu and tutorial outline.

---
+++ IconExplorer/NumericHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace IconExplorer
+{
+    public class NumericHelper
+    {
+        public static string Loop2ICX(string bigInt, bool trimTrailing = true)
+        {
+
+            BigInteger icxloop = BigInteger.Parse(bigInt);
+
+            BigInteger numericBase = BigInteger.Parse("10");
+
+            BigInteger result = icxloop / BigInteger.Pow(numericBase, 18);
+
+            return result.ToString();
+        }
+    }
+}
